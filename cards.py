98d1e505286e296782dc9f85641e81b4d32f3a23
Julian Jung <tick.73spatial@icloud.com>
2025-07-10 13:55:57 +0200

    Update cards.py
    high card plays for 5 rounds

--- cards.py
+++ cards.py
@@ -63,30 +63,49 @@ class Player:
     def __init__(self, name:str="John Doe") -> None:
         self.name = name
         self.hand = []
+        self.score = 0
 
     def show_hand(self) ->str:
         return "\n".join(str(card) for card in self.hand)
 
-def highCard():
-    p1 = Player("Julian")
-    p2 = Player("Not-Julian")
-    d1 = Deck()
+def highCard(p1, p2, d1):
     d1.shuffle()
     c1 = d1.draw_card()
     c2 = d1.draw_card()
+    
+    print(f"{p1.name} draws: {c1}")
     p1.hand.append(c1)
-    p2.hand.append(c2)
-
-    print(f"{p1.name} draws: " + p1.show_hand())
     print(f"{p2.name} draws: {c2}")
+    p2.hand.append(c2)
 
     if c1.get_value() > c2.get_value():
         print(f"{p1.name} wins!")
+        p1.score += 1
     elif c1.get_value() < c2.get_value():
         print(f"{p2.name} wins!")
+        p2.score += 1
     else:
         print("Draw!")
 
+def main():
+    p1 = Player("Julian")
+    p2 = Player("Not-Julian")
+    d1 = Deck()
+
+    for i in range(5):
+        highCard(p1, p2, d1)
+        print("-----")
+    print(f"{p1.name}: {p1.score} Points")
+    print(f"{p2.name}: {p2.score} Points")
+
+    if p1.score > p2.score:
+        print(f"{p1.name} is the winner")
+    elif p1.score < p2.score:
+        print(f"{p2.name} is the winner!")
+    else:
+        # Need to check if possible
+        print("No winner could be dertermined")
+
 
 if __name__ == "__main__":
-    highCard()
+    main()
