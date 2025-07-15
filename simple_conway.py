b00b1685289906c2da763ccba7b2656f7b73fc6c
Julian Jung <tick.73spatial@icloud.com>
2025-07-15 09:40:22 +0200

    Update simple_conway.py
    Working Code

--- simple_conway.py
+++ simple_conway.py
@@ -1,7 +1,7 @@
 import time
 
 def create_grid(x, y):
-    return [[0 for i in range(10)] for j in range(10)]
+    return [['.' for i in range(10)] for j in range(10)]
 
 def print_grid(grid):
     for row in grid:
@@ -26,15 +26,33 @@ def count_neighbors(grid, x_pos, y_pos):
                     count += 1
     return count
 
+def next_gen(current_grid):
+    rows = len(current_grid)
+    cols = len(current_grid[0])
+    new_grid = [['.' for _ in range(cols)] for _ in range(rows)]
+
+    for x in range(rows):
+        for y in range(cols):
+            neighbors = count_neighbors(current_grid,x,y)
+            if current_grid[x][y] == 1 and neighbors in [2, 3]:
+                new_grid[x][y] = 1 # stays alive
+                # print(f"Next Gen of {x},{y}: Alive")
+            elif current_grid[x][y] == '.' and neighbors == 3:
+                new_grid[x][y] = 1 # born
+                # print(f"Next Gen of {x},{y}: Born")
+            else:
+                new_grid[x][y] = '.' # dies
+                # print(f"Next Gen of {x},{y}: Dead") 
+    return new_grid
 
 if __name__ == "__main__":
 
-    cells = [(1, 1), (2, 2)]
+    cells = [(1, 1), (1, 2), (1, 3)]
     g1 = create_grid(10,10)
     spawn_cell(cells, g1)
-    print_grid(g1)
 
-    for row in range(len(g1)):
-        for col in range(len(g1[1])):
-            count_neighbors(g1, row, col)
-            print(f"{count_neighbors(g1, row, col)} at {row}, {col}")
+    for i in range(10):
+        print(f"\nGeneration {i}")
+        print_grid(g1)
+        g1 = next_gen(g1)
+        time.sleep(1)
